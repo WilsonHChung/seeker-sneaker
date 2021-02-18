8683e89cc3bfc56318fcba86b9deff3404a815c4
Wilson Chung <wilsonhchung@gmail.com>
2021-02-17 20:58:19 -0800

    Initial commit of seeker-sneaker project

---
+++ seeker-sneaker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  seeker-sneaker
+//
+//  Created by Wilson Chung on 2/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ seeker-sneaker/seeker_sneakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  seeker_sneakerApp.swift
+//  seeker-sneaker
+//
+//  Created by Wilson Chung on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct seeker_sneakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
