96a1d7cd7ca0af92df7a4b3c08eff4bb5c4932df
Dominic Gutierrez <dominic.gutierrez@sjsu.edu>
2021-03-09 21:02:23 -0800

    added search bar

--- seeker-sneaker/FeedView.swift
+++ seeker-sneaker/FeedView.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct FeedView: View {
+    @State var text = ""
     var body: some View {
-        GeometryReader { bounds in
-            ScrollView {
-                LazyVGrid (columns:
-                            [GridItem(.adaptive(minimum: bounds.size.width / 3 - 1.2), spacing: 1.2)],
-                           spacing: 1) {
-                    ForEach(feedData) {
-                        item in
-                        ImagesView(feed: item)
-                            .frame(width: bounds.size.width / 3 - 0.6, height: bounds.size.width / 3 - 0.6)
+        VStack{
+            SearchBar(text: $text)
+            GeometryReader { bounds in
+                ScrollView {
+                    LazyVGrid (columns:
+                                [GridItem(.adaptive(minimum: bounds.size.width / 3 - 1.2), spacing: 1.2)],
+                               spacing: 1) {
+                        ForEach(feedData.filter({"\($0)".contains(text.lowercased()) || text.isEmpty})) {
+                            item in
+                            ImagesView(feed: item)
+                                .frame(width: bounds.size.width / 3 - 0.6, height: bounds.size.width / 3 - 0.6)
+                        }
                     }
                 }
             }
@@ -30,5 +34,70 @@ struct FeedView_Previews: PreviewProvider {
         FeedView()
     }
 }
+
+//struct SearchBar: View {
+//    @Binding var searchText: String
+//    @Binding var isSearching: Bool
+//    @Binding var showResults: Bool
+//    @Binding var loadSearch: Bool
+//
+//    var body: some View {
+//
+//        HStack (spacing: 0) {
+//            HStack{
+//            TextField("Search", text: $searchText)
+//                .padding(.leading, 24)
+//            }
+//            .frame(height: 4)
+//            .padding()
+//            .background(Color(.systemGray5))
+//            .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+//            .padding(.horizontal)
+//            .onTapGesture(perform: {
+//                isSearching = true
+//                showResults = true
+//            })
+//            .overlay {
+//                HStack {
+//                    Image(systemName: "magnifyingglass")
+//                    Spacer()
+//
+//
+//                    Button(action: {searchText = ""},
+//                    label: {
+//                        Image(systemName: "xmark.circle.fill")
+//                            .padding()
+//                            .foregroundColor(.gray)
+//                            .opacity(isSearching ? 1 : 0)
+//                    })
+//                }
+//                .padding(.horizontal, 32)
+//                .foregroundColor(.gray)
+//            }
+//            .animation(loadSearch ? .spring() : .none)
+//
+//            if isSearching {
+//                Button(action: {
+//                    isSearching = false
+//                    searchText = ""
+//                    showResults = false
+//
+//                    UIApplication.shared
+//                        .sendAction(#selector(UIResponder .resignFirstResponder), to: nil, from: nil, for: nil)
+//                },
+//                label: {
+//                    Text("Cancel")
+//                        .padding(.trailing)
+//                        .padding(.leading, 0)
+//                }
+//                )
+//                .transition(.move(edge: .trailing))
+//                .animation(.spring())
+//            }
+//        }
+//    }
+//}
+
+
 
 
---
+++ seeker-sneaker/SearchBar.swift
@@ -0,0 +1,54 @@
+//
+//  SearchBar.swift
+//  seeker-sneaker
+//
+//  Created by Dominic Gutierrez on 3/3/21.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var text: String
+    @State private var isEditing = false
+    var body: some View {
+        HStack {
+            TextField("Search", text: $text)
+                .padding(15)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .foregroundColor(.black)
+                .cornerRadius(12)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+                            }, label: {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            })
+                        }
+                    }
+                ).onTapGesture {
+                    self.isEditing = true
+                }
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }){
+                    Text("Done")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+    }
+}
